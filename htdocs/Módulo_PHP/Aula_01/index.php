45ef57d3750afbdaf8bd94e930ac4dddc3c11a7d
Guilherme Branco <87778836+gbranco0211@users.noreply.github.com>
2021-08-25 11:27:39 -0300

    Add files via upload

---
+++ htdocs/Módulo_PHP/Aula_01/index.php
@@ -0,0 +1,22 @@
+<html>
+	
+	<head>
+		<title>Minha página de teste</title>
+		<meta charset="uft-8">
+
+
+	</head>
+	<body>
+		
+		<p>
+			<?php
+
+				echo "Olá Mundo";
+
+			?>
+		</p>
+
+	</body>
+
+
+</html>
---
+++ htdocs/Módulo_PHP/Aula_2/index.php
@@ -0,0 +1,10 @@
+<?php
+
+	echo '<pre>',
+	print_r($_SERVER);
+	echo '</pre>';
+
+
+
+
+?>
